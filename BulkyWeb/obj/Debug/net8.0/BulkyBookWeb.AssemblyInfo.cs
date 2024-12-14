f0d18439343b3b77eaf9398bdeab45a2a0e90c01
HongAn <nguyenhongan.tphcm@gmail.com>
2024-12-14 13:10:32 +0700

    Update OG meta

--- BulkyWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4445e854b1ab75d228aa6d69e8e2e805f0b676e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bbb35c59fbad9577b0e8722f6d75d30860f303")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
